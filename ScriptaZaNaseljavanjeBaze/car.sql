315cdd70432816c492890b64726a5a9508917cbe
veljkoOrdic <132483539+veljkoOrdic@users.noreply.github.com>
2023-05-03 22:10:35 +0200

    Add files via upload

---
+++ ScriptaZaNaseljavanjeBaze/car.sql
@@ -0,0 +1,7 @@
+INSERT INTO java_parking_application.car (registration_number,brand,model) VALUES
+	 ('1','1','1'),
+	 ('BG102OP','Lancia','Delta'),
+	 ('BG332SU','Audi','A3'),
+	 ('NI409II','Fiat','Tipo'),
+	 ('NS551TT','Nissan','Micra'),
+	 ('test','test','test');
---
+++ ScriptaZaNaseljavanjeBaze/login.sql
@@ -0,0 +1,13 @@
+INSERT INTO java_parking_application.login (username,password) VALUES
+	 ('veljko','veljko123'),
+	 ('velja','velja123'),
+	 ('test','test123'),
+	 ('opetTest','123'),
+	 ('testOpetNeki','123'),
+	 ('pismo','123'),
+	 ('KonacniTest','123'),
+	 ('aleksa','123'),
+	 ('1','1'),
+	 ('2','2');
+INSERT INTO java_parking_application.login (username,password) VALUES
+	 ('55','1');
